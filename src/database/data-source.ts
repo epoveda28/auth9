f5d62d43b815970065b6700c2ea67b8439733508
Edel Poveda <epoveda28@gmail.com>
2026-08-18 17:14:25 -0500

    primera migración

---
+++ src/database/data-source.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+
+export default new DataSource({
+  type: 'postgres',
+
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+
+  entities: [User],
+
+  migrations: ['src/database/migrations/*.{ts,js}'],
+
+  synchronize: false,
+});
